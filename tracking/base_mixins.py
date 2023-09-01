a7a00e18fc57b36329e083aa9de87a6b0dc71f70
mahdi <mahdipanahimpui@gmail.com>
2023-09-01 22:19:22 +0330

    fix the last commit

--- tracking/base_mixins.py
+++ tracking/base_mixins.py
@@ -1,6 +1,18 @@
+from django.utils.timezone import now
 
 
 
 
 class BaseLoggingMixin:
-    pass
+
+    def initial(self, request, *args, **kwargs):
+        print('helllllo')
+        # super() called the initial of the APIView(not the BaseLoggingMixin) in the Home view in views.py
+        return super().initial(request, *args, **kwargs) 
+
+
+
+    def finalize_response(self, request, response,*args, **kwargs):
+
+        # super() called the finalize_response of the APIView(not the BaseLoggingMixin) in the Home view in views.py
+        return super().finalize_response(request, response,*args, **kwargs)
