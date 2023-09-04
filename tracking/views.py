2b0a1535b80725043499c63c5a171cab4578278f
mahdi <mahdipanahimpui@gmail.com>
2023-09-04 14:44:12 +0330

    could override the should_log

--- tracking/views.py
+++ tracking/views.py
@@ -7,7 +7,10 @@ class Home(LoggingMixin, APIView):
     # Note APIView have the initial and finalize_response too
     # if call the super() in the initial and finalize_response of the LoggingMixin, the initial and finalize_response of the second calss(APIView) is called
     
-    logging_methods = ['GET', 'POST'] # overriding the logging_methods, <just log the get and post method>
+    # logging_methods = ['GET', 'POST'] # overriding the logging_methods, <just log the get and post method>
+
+    # def should_log(self, request, response): # could override the shoul_log
+    #     return response.satatus_code >= 400
 
     def get(self, request):
         return Response('hello')
